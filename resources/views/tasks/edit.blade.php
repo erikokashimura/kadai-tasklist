64592a455be9a8ddc4990dd11f7d42448dff2091
eriko.kashimura <eriko.kashimura@rakuten.com>
2018-05-31 06:47:48 +0000

    add column

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -5,7 +5,10 @@
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
     
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-
+        
+        {!! Form::label('status', '進捗状況:') !!}
+        {!! Form::text('status') !!}
+        
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
 
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -3,8 +3,9 @@
 @section('content')
 
     <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
-
-    <p>{{ $task->content }}</p>
+    
+    <p>進捗状況: {{ $task->status }}</p>
+    <p>タスク: {{ $task->content }}</p>
 
     {!! link_to_route('tasks.edit', 'このタスク編集', ['id' => $task->id]) !!}
 
