a21c1eed08fe736ae83b084f79f5aa95838fb7bb
eriko.kashimura <eriko.kashimura@rakuten.com>
2018-06-04 01:03:09 +0000

    bootstrap

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -4,13 +4,30 @@
 
     <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
     
+    <div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-la-sm-offset-3 col-la-8">
+    
     <p>進捗状況: {{ $task->status }}</p>
-    <p>タスク: {{ $task->content }}</p>
-
-    {!! link_to_route('tasks.edit', 'このタスク編集', ['id' => $task->id]) !!}
-
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $task->id }}</td>
+        </tr>
+        <tr>
+            <th>進捗状況</th>
+            <td>{{ $task->status }}</td>
+        </tr>
+        <tr>
+            <th>タスク</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+    </table>
+    
+    {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
+    
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
-        {!! Form::submit('削除') !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
-
+        </div>
+    </div>
 @endsection
